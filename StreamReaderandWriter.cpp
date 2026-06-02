aa9b019f56b5041aa45c1361242ab2a198dd0a75
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-06-02 18:21:18 +0700

    edit

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -14,7 +14,7 @@ int main()
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
-    /unlimated loop untuk menulis file
+    //unlimated loop untuk menulis file
     while (true)
     {
 
@@ -27,3 +27,29 @@ int main()
         outfile << baris << endl;
 
     }
+
+    //menutup file setelah penulisan selesai
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file " << endl;
+    //kondisi jika file ada 
+    if (infile.is_open())
+    {
+        //perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+         //menutup file jika telah selesai
+        infile.close();
+    }
+
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file" ;
+    return 0;
+    
+}
